9f69133822de55ba30876423c9353dbb6120e466
GR <135833553+GertPA@users.noreply.github.com>
2024-03-31 16:21:59 +0200

    Create 5. Magic 8 Ball.cs

---
+++ BeginnerProjects/5. Magic 8 Ball.cs
@@ -0,0 +1,35 @@
+//Magic 8 ball
+
+Random randomNr = new Random();
+int bigBrain = randomNr.Next(0,3);
+int randomPos = randomNr.Next(0, 11);
+int randomNeg = randomNr.Next(0,6);
+
+
+//This looks ugly af,but it works... find a way to fix it 
+string[] positiveAnswer={"It is certain", "It is decidedly so","Without a doubt","Yes definitely","You may rely on it", "As I see it, yes", "Mo st likely","Outlook good","Yes","Signs point to yes" };
+string[] neutralAnswer ={ "Try again","Ask again later"," Better not tell you now"," Cannot predict now" ," Concentrate and ask again"};
+string[] negativeAnswer ={"Don't count on it","My reply is no","My sources say no","Outlook not so good,Very doubtful"};
+
+
+Console.WriteLine("Ask the mighty 8 ball your question :");
+string userQuestion = Console.ReadLine();
+
+switch(bigBrain)
+{
+    case 0:
+    Console.WriteLine($"The mighty 8 balls says :: {positiveAnswer[randomPos]}"); //    print (answers[random.randint(0, len(answers)-1)] ) doing something like this ...
+    break;
+
+   case 1:
+   Console.WriteLine($"The mighty 8 balls says :: {neutralAnswer[randomNeg]}");
+   break;   
+
+   default:
+   Console.WriteLine($"The mighty 8 balls says :: {negativeAnswer[randomNeg]}");
+   break;
+
+}
+
+
+
