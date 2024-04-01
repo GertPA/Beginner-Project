ab044808b4439995c8852fffedb1cb9b09e5734f
GR <135833553+GertPA@users.noreply.github.com>
2024-04-01 09:17:43 +0200

    Update 5. Magic 8 Ball.cs

--- BeginnerProjects/5. Magic 8 Ball.cs
+++ BeginnerProjects/5. Magic 8 Ball.cs
@@ -1,9 +1,9 @@
 //Magic 8 ball
 
 Random randomNr = new Random();
-int bigBrain = randomNr.Next(0,3);
+int answerList = randomNr.Next(0,3);
 int randomPos = randomNr.Next(0, 11);
-int randomNeg = randomNr.Next(0,6);
+int randomNeg = randomNr.Next(0,6);//randomNr.Next(0,negativeAnswer.Length-1) might wrk too
 
 
 //This looks ugly af,but it works... find a way to fix it 
@@ -12,24 +12,25 @@ string[] neutralAnswer ={ "Try again","Ask again later"," Better not tell you no
 string[] negativeAnswer ={"Don't count on it","My reply is no","My sources say no","Outlook not so good,Very doubtful"};
 
 
-Console.WriteLine("Ask the mighty 8 ball your question :");
-string userQuestion = Console.ReadLine();
+Console.WriteLine("Ask the mighty 8 ball your question:");
 
-switch(bigBrain)
+string? userQuestion = Console.ReadLine();
+
+
+switch(answerList)
 {
     case 0:
-    Console.WriteLine($"The mighty 8 balls says :: {positiveAnswer[randomPos]}"); //    print (answers[random.randint(0, len(answers)-1)] ) doing something like this ...
+    Console.WriteLine($"The mighty 8 ball says: {positiveAnswer[randomPos]}");
     break;
 
    case 1:
-   Console.WriteLine($"The mighty 8 balls says :: {neutralAnswer[randomNeg]}");
+   Console.WriteLine($"The mighty 8 ball says: {neutralAnswer[randomNeg]}");
    break;   
 
    default:
-   Console.WriteLine($"The mighty 8 balls says :: {negativeAnswer[randomNeg]}");
+   Console.WriteLine($"The mighty 8 ball says: {negativeAnswer[randomNeg]}");
    break;
 
 }
-
 
 
