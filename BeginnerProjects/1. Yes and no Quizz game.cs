fb3db0f3182efd708c01b10d3a88fe6df31e924a
GR <135833553+GertPA@users.noreply.github.com>
2024-04-24 18:39:04 +0200

    Update 1. Yes and no Quizz game.cs
    i learned classes , tried to apply it here ... i think it works

--- BeginnerProjects/1. Yes and no Quizz game.cs
+++ BeginnerProjects/1. Yes and no Quizz game.cs
@@ -1,53 +1,68 @@
+
 //Question to points
 
-int score = 0;
+Quizz a = new Quizz();
+Console.WriteLine(a); //this works i think
 
-Console.WriteLine("Welcome to this quiz! Answer with \"yes\" or \"no\". Do you wish to continue?");
-Console.ReadLine();
 
-Console.WriteLine("Lungs are the largest internal organ in the human body."); 
-string? firstQuestion = Console.ReadLine();
-string firsQuestionStatus = "no"; 
-CheckingMethod(firstQuestion, firsQuestionStatus);
-
-Console.WriteLine(" The first song ever sung in the space was “Happy Birthday.”");  
-string? secondQuestion = Console.ReadLine();
-string secondQuestionStatus = "yes";
-CheckingMethod(secondQuestion, secondQuestionStatus);
-
-Console.WriteLine("A male canary tends to have a better singing voice than a female canary."); 
-string? thirdQuestion = Console.ReadLine();
-string thirdQuestionStatus = "yes";
-CheckingMethod(thirdQuestion, thirdQuestionStatus);
-
-Console.WriteLine("Dolphin is considered to be the fastest species in the ocean."); 
-string? forthQuestion = Console.ReadLine();
-string forthQuestionStatus = "no";
-CheckingMethod(forthQuestion, thirdQuestionStatus);
-
-Console.WriteLine("Mice have more bones than humans."); 
-string? fifthQuestion = Console.ReadLine();
-string fifthQuestionStatus = "yes";
-CheckingMethod(fifthQuestion, fifthQuestionStatus);
-
-void CheckingMethod(string userInputedWord, string questionStatus) //will check if the answer is correct or nah 
-{
-    if (userInputedWord.ToLower() == questionStatus)
+    class Quizz
     {
-        Console.WriteLine("+1 good job");
-        score++;
+
+        int score = 0;
+        public Quizz()
+        {
+            Console.WriteLine("Welcome to this quiz! Answer with \"yes\" or \"no\". Do you wish to continue?");
+            Console.ReadLine();
+
+            Console.WriteLine("Lungs are the largest internal organ in the human body.");
+            string? firstQuestion = Console.ReadLine();
+            string firsQuestionStatus = "no";
+            CheckingMethod(firstQuestion, firsQuestionStatus);
+
+            Console.WriteLine(" The first song ever sung in the space was “Happy Birthday.”");
+            string? secondQuestion = Console.ReadLine();
+            string secondQuestionStatus = "yes";
+            CheckingMethod(secondQuestion, secondQuestionStatus);
+
+            Console.WriteLine("A male canary tends to have a better singing voice than a female canary.");
+            string? thirdQuestion = Console.ReadLine();
+            string thirdQuestionStatus = "yes";
+            CheckingMethod(thirdQuestion, thirdQuestionStatus);
+
+            Console.WriteLine("Dolphin is considered to be the fastest species in the ocean.");
+            string? forthQuestion = Console.ReadLine();
+            string forthQuestionStatus = "no";
+            CheckingMethod(forthQuestion, thirdQuestionStatus);
+
+            Console.WriteLine("Mice have more bones than humans.");
+            string? fifthQuestion = Console.ReadLine();
+            string fifthQuestionStatus = "yes";
+            CheckingMethod(fifthQuestion, fifthQuestionStatus);
+
+            var calculatingSCore = score >= 3 ? $"good job you scored {score}/3 whih is perfect" : $"well at least your tried your score is {score}";
+            Console.WriteLine(calculatingSCore);
+
+        }
+        void CheckingMethod(string userInputedWord, string questionStatus) //will check if the answer is correct or nah 
+        {
+            if (userInputedWord.ToLower() == questionStatus)
+            {
+                Console.WriteLine("+1 good job");
+                score++;
+            }
+
+            else if (userInputedWord.ToLower() != questionStatus)
+            {
+                Console.WriteLine("Wrong");
+            }
+
+            else
+            {
+                Console.WriteLine("invalid input , select yes or no ");
+            }
+        }
     }
 
-    else if (userInputedWord.ToLower() != questionStatus)
-    {
-        Console.WriteLine("Wrong");
-    }
 
-    else
-    {
-        Console.WriteLine("invalid input , select yes or no ");
-    }
-}
 
-var calculatingSCore = score >= 3 ? $"good job you scored {score}/3 whih is perfect" : $"well at least your tried your score is {score}";
-Console.WriteLine(calculatingSCore);
+
