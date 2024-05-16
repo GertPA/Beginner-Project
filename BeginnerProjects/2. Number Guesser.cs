08370efff342624c2c324f33caaa6b30b2b8366e
GR <135833553+GertPA@users.noreply.github.com>
2024-05-16 11:13:53 +0200

    Update 2. Number Guesser.cs

--- BeginnerProjects/2. Number Guesser.cs
+++ BeginnerProjects/2. Number Guesser.cs
@@ -1,84 +1,94 @@
-//Guess the number 1-100, hints when its above or below your guess\
-
-Random randomNumber = new Random();
-int randomNr = randomNumber.Next(1, 101);
-bool correctAwnser = true;
-int numberOfLives = 5;
-int numberOfGuesses = 0;
-int higherNumber = 0;
-int lowerNumber = 0;
-Console.WriteLine($"Hello and welcome to the guessing game \n Pick your game mode:");
-Console.WriteLine($"[1] - Normal Mode\n[2] - Survival Mode");
-
-
-int userPicked = Convert.ToInt32(Console.ReadLine());
-if (userPicked == 1) // Normal Mode
+class GuessTheNumber
 {
-    while (correctAwnser == true)
+  public Random randomNumber = new Random();
+  private int randomNr;
+  private bool correctAwnser = true;
+  private int numberOfLives { get; set; }
+  private int numberOfGuesses { get; set; }
+  private int higherNumber { get; set; }
+  private int lowerNumber { get; set; }
+  public GuessTheNumber(int _numberOfLives, int _numberOfGuesses, int _higherNumber, int _lowerNumber)
+  {
+    randomNr = randomNumber.Next(1, 101);
+
+    Console.WriteLine($"Hello and welcome to the guessing game \n Pick your game mode:");
+    Console.WriteLine($"[1] - Normal Mode\n[2] - Survival Mode");
+    int userPicked = Convert.ToInt32(Console.ReadLine());
+    if (userPicked == 1) // Normal Mode
     {
+      while (correctAwnser == true)
+      {
         Console.WriteLine("Guess a number between 1 and 100");
         int userInput = Convert.ToInt32(Console.ReadLine());
 
         if (randomNr > userInput)
         {
-            Console.WriteLine("Its higher");
-            numberOfGuesses++;
-            higherNumber++;
-            correctAwnser = true;
+          Console.WriteLine("Its higher");
+          _numberOfGuesses++;
+          _higherNumber++;
+          correctAwnser = true;
         }
         else if (randomNr < userInput)
         {
-            Console.WriteLine("its lower");
-            lowerNumber++;
-            numberOfGuesses++;
-            correctAwnser = true;
+          Console.WriteLine("its lower");
+          _lowerNumber++;
+          _numberOfGuesses++;
+          correctAwnser = true;
         }
         else if (randomNr == userInput)
         {
-            Console.WriteLine($"Good job you found it, {randomNr} was the secret number");
-            correctAwnser = false;
+          Console.WriteLine($"Good job you found it, {randomNr} was the secret number");
+          correctAwnser = false;
         }
+      }
     }
-}
-else //Survival Mode
-{
-    while (correctAwnser == true)
+    else //Survival Mode
     {
+      while (correctAwnser == true)
+      {
 
         Console.WriteLine("Guess a number between 1 and 100");
         int userInput = Convert.ToInt32(Console.ReadLine());
 
-        if (randomNr > userInput && numberOfLives > 0)
+        if (randomNr > userInput && _numberOfLives > 0)
         {
-            Console.WriteLine("Its higher");
-            numberOfGuesses++;
-            higherNumber++;
-            numberOfLives--;
-            correctAwnser = true;
+          Console.WriteLine("Its higher");
+          _numberOfGuesses++;
+          _higherNumber++;
+          _numberOfLives--;
+          correctAwnser = true;
         }
-        else if (randomNr < userInput && numberOfLives > 0)
+        else if (randomNr < userInput && _numberOfLives > 0)
         {
-            Console.WriteLine("its lower");
-            lowerNumber++;
-            numberOfGuesses++;
-            numberOfLives--;
-            correctAwnser = true;
+          Console.WriteLine("its lower");
+          _lowerNumber++;
+          _numberOfGuesses++;
+          _numberOfLives--;
+          correctAwnser = true;
         }
         else if (randomNr == userInput)
         {
-            Console.WriteLine($"Good job you found it, {randomNr} was the secret number");
-            correctAwnser = false;
+          Console.WriteLine($"Good job you found it, {randomNr} was the secret number");
+          correctAwnser = false;
         }
-        else if (numberOfLives == 0) 
+        else if (_numberOfLives == 0)
         {
-            Console.WriteLine($"Game over! You ran out of lives.");
-            correctAwnser = true;
-            break;
+          Console.WriteLine($"Game over! You ran out of lives.");
+          correctAwnser = true;
+          break;
         }
 
+      }
     }
+    Console.WriteLine($"Total Number of guesses is: {_numberOfGuesses} , with {_higherNumber} guesses being higher numbers and {_lowerNumber} guesses being lower numbers");
+  }
+
 }
 
-Console.WriteLine($"Total Number of guesses is: {numberOfGuesses} , with {higherNumber} guesses being higher numbers and {lowerNumber} guesses being lower numbers");
-
-
+class Program
+{
+  public static void Main()
+  {
+    GuessTheNumber run = new GuessTheNumber(5, 0, 0, 0);
+  }
+}
