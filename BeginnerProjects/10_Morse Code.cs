2af6da614ae97f515ec37300ed195d16d5db529b
GR <135833553+GertPA@users.noreply.github.com>
2024-05-24 13:18:00 +0200

    Update 10_Morse Code.cs

--- BeginnerProjects/10_Morse Code.cs
+++ BeginnerProjects/10_Morse Code.cs
@@ -1,52 +1,49 @@
 //THIS TOOK SO MUCH GOD DAMN  TIME TO FIGURE IT OUT , ITS SO FUCKING SIMPLE YET I COULNT FIGURE IT OUT WITHOUT HELP , I AINT CUT OUT FOR THIS 
 
-class MorseConverter
+class MorseConverter //Main class Maybe we can seperate it ?
 {
   string[] morseCode = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."];
   char[] alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  
 
-
+ 
   public static void Main()
   {
-    MorseConverter morseconv = new MorseConverter();
+    MorseConverter morseconv = new MorseConverter();//used to call the class
+    Console.WriteLine("You wanna convert : \n1.Text To Morse \n2.Morse to Text");
+    int? userChoice = Convert.ToInt16(Console.ReadLine()); //get user input for the mode , converts it to int
+
     Console.WriteLine("what do you want to say?");
     string? userInput = Console.ReadLine();
 
-    if (userInput != null) //so we can remove that damn annoying null reference warning
+    switch (userChoice)
     {
-      foreach (char item in userInput.ToUpper())
-      {
-        for (int i = 0; i < morseconv.alphabet.Length; i++)
+      case 1://Text To Morse
+        foreach (char item in userInput.ToUpper()) //for each character in userInput , plus  transfroming it to upperCase
         {
-          if (morseconv.alphabet[i] == item)
+          for (int i = 0; i < morseconv.alphabet.Length; i++) // "i"- will become the length of the array 
           {
-            Console.Write(" " + morseconv.morseCode[i] + " ");
-
+            if (morseconv.alphabet[i] == item) //check if the letter is the same with the user character
+                Console.Write(" " + morseconv.morseCode[i] + " "); // if it is i will become that character order , pasting it to the more array .
           }
         }
-      }
-    }
-    else
-    {
-      Console.WriteLine("No input was provided.");
-    }
+        break;
 
-  }
+      case 2: //Morse To Text
+        //  foreach (char item in userInput.ToUpper()) //only works for singe character , there must be a way to make it work with setences
+        //  {
+         for (int i = 0; i < morseconv.morseCode.Length; i++)
+         {
+          if(morseconv.morseCode[i] == userInput) 
+           Console.WriteLine(morseconv.alphabet[i]);
+         } 
+        //  }
+        break;
 
-
-  void Calculate(char[] alp, string[] morse, char it)
-  {
-
-    for (int i = 0; i < alphabet.Length; i++)
-    {
-      if (alphabet[i] == it)
-      {
-        Console.Write(" " + morseCode[i] + " ");
-
-      }
+      default:
+        Console.WriteLine("No input was provided.");
+        break;
     }
 
   }
-
-
 }
