d65acd7b34f345a2dbbb7e2fba6577e92e9d7c05
GR <135833553+GertPA@users.noreply.github.com>
2024-06-07 11:15:45 +0200

    Update 14_HangMan.cs

--- BeginnerProjects/14_HangMan.cs
+++ BeginnerProjects/14_HangMan.cs
@@ -4,31 +4,44 @@ string[] listOfWord =["abstract","accounts","activation","afternoon","justice"];
 Random random = new Random();
 int randomSelection = random.Next(0,listOfWord.Length);
 
-while (true)
-{
-  string userInput = Console.ReadLine();
+//   string userInput = Console.ReadLine()!;
 
-        for (int i = 0; i < userInput.Length; i++)
+do
+{
+ Console.WriteLine("Enter your guess:");
+ string userInput = Console.ReadLine()!;
+    CheckingHangman(userInput,listOfWord);        
+} while (found);
+    
+void CheckingHangman(string userInput,string[] listOfWord)
+{
+        found = false; // Reset found for this iteration
+        bool correctGuess = false;
+
+         for (int i = 0; i < userInput.Length; i++)
          {
                               
                
                 if (i < listOfWord[randomSelection].Length && userInput[i] == listOfWord[randomSelection][i])
                 {
-                        Console.Write($"{ listOfWord[randomSelection][i]}");
+                        Console.Write($"{listOfWord[randomSelection][i]}");
+                        correctGuess = true;
                         
                         if(userInput == listOfWord[randomSelection])
                         {
-                                Console.WriteLine("found it");
-                                found = false; 
-                                break;
+                                Console.WriteLine("nice");
+                                found = false; // End the game
+                               break;
+                               
                         }
 
         
                 }
                 else
                 {
-                        Console.Write("_");
+                        Console.Write("_ ");
                 }
-        }      
+         }
+      
 }
-
+   
