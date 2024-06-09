1a090e849c175239a2a02498beb003688e2180cc
GR <135833553+GertPA@users.noreply.github.com>
2024-06-09 10:25:52 +0200

    Update 06_CalculateTime.cs

--- BeginnerProjects/06_CalculateTime.cs
+++ BeginnerProjects/06_CalculateTime.cs
@@ -1,51 +1,29 @@
-Console.WriteLine("The following items are available:");
-Console.WriteLine(" 1 – Rope\n 2 – Torches\n 3 – Climbing Equipment\n 4 – Clean Water\n 5 – Machete\n 6 – Canoe\n 7 – Food Supplies");
+//Instead of Prasing it now we can do it later
 
-Console.WriteLine("Sup boss what can i get you ??");
-int userInput = Convert.ToInt32(Console.ReadLine());
-PriceLinst();
+Console.WriteLine("How many hours have you played");
+int playedHours = int.Parse(Console.ReadLine());
+Console.WriteLine("How many minutes have you played?");
+int playedMinutes = int.Parse(Console.ReadLine());
+  
+int totalH = 8;
+int totalM = 30;
+ 
 
-Console.WriteLine("Anything else (y or n)");
-string letsTry = Console.ReadLine();
+int calcH = playedHours + totalH;
+int calcM = playedMinutes + totalM;
+int differM = 0;
+ 
 
-// if (letsTry == "y") //doesnt work
-
-// {
-//     PriceLinst();
-// }
-// else{
-//     Console.WriteLine(">?");
-// }
-
-
-void PriceLinst()
+if (calcM >= 60)
 {
-           switch (userInput)
+    differM = calcM - 60;
+    calcH += 1;
+}
+
+else
 {
-    case 1:
-    Console.WriteLine("Rope cost 10 gold.");
-    break; 
-        case 2:
-    Console.WriteLine("Torches cost 15 gold.");
-    break;
-        case 3:
-    Console.WriteLine("Climbing Equipment cost 25 gold.");
-    break;
-        case 4:
-    Console.WriteLine("Clean Water cost 1 gold.");
-    break;
-        case 5:
-    Console.WriteLine("Machete cost 20 gold.");
-    break;
-        case 6:
-    Console.WriteLine("Canoe cost 200 gold.");
-    break;
-        case 7:
-    Console.WriteLine("Food Supplies cost 1 gold.");
-    break;
-    
-    default:
-    Console.WriteLine("Invalid Input pls type a number ...");//???
-    break;
+    differM = totalM +playedMinutes ;
 }
-}
+   
+Console.WriteLine($"Hours : {calcH} ");
+Console.WriteLine($"Minutes: {differM}");
